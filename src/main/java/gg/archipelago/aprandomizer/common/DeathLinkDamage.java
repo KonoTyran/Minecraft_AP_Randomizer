5c667c09cc06771e27b69246d6c09698cbb5e6c9
Joshua Jones <joshuajones2010@gmail.com>
2024-01-18 19:50:33 +1100

    Cleanup unnecessary change

--- src/main/java/gg/archipelago/aprandomizer/common/DeathLinkDamage.java
+++ src/main/java/gg/archipelago/aprandomizer/common/DeathLinkDamage.java
@@ -1,20 +1,15 @@
 package gg.archipelago.aprandomizer.common;
 
 import gg.archipelago.aprandomizer.APRandomizer;
-import gg.archipelago.aprandomizer.common.Utils.Utils;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.LivingEntity;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.Set;
 
 public class DeathLinkDamage extends DamageSource {
 
@@ -28,27 +23,10 @@ public class DeathLinkDamage extends DamageSource {
 
     public static ResourceKey<DamageType> DEATH_LINK = ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation(APRandomizer.MODID,"death_link"));
 
-    private static class DamageTypeWrapper {
-        public Holder.Reference<DamageType> damageType;
-        DamageTypeWrapper() {
-            this.damageType = APRandomizer.getServer().registryAccess().lookupOrThrow(Registries.DAMAGE_TYPE).getOrThrow(DeathLinkDamage.DEATH_LINK);
-            // Don't know if this is explicitly needed considering the damage value, but it can't hurt.
-            this.damageType.bindTags(Set.of(
-                    DamageTypeTags.BYPASSES_INVULNERABILITY,
-                    DamageTypeTags.BYPASSES_COOLDOWN,
-                    DamageTypeTags.BYPASSES_RESISTANCE,
-                    DamageTypeTags.BYPASSES_ARMOR,
-                    DamageTypeTags.BYPASSES_EFFECTS,
-                    DamageTypeTags.BYPASSES_ENCHANTMENTS,
-                    DamageTypeTags.BYPASSES_SHIELD,
-                    DamageTypeTags.AVOIDS_GUARDIAN_THORNS
-            ));
-        }
-    }
-    private static final DamageTypeWrapper damageType = new DamageTypeWrapper();
+    private static final Holder<DamageType> damageType = APRandomizer.getServer().registryAccess().lookupOrThrow(Registries.DAMAGE_TYPE).getOrThrow(DeathLinkDamage.DEATH_LINK);
 
     public DeathLinkDamage() {
-        super(damageType.damageType);
+        super(damageType);
     }
 
     @Override
